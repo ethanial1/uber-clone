b4605b04d3a454a8f190836680ae2a8182f3a2d7
ethanial1 <camiloeiz00@gmail.com>
2022-02-24 12:01:27 -0600

    implementación de iconos

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
+import { Icon } from 'react-native-elements';
 import tw from 'twrnc';
 
 const data = [
@@ -23,7 +24,8 @@ const NavOptions = () => {
         <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2`}>
           <View>
             <Image style={styles.itemImage} source={{url: item.image}} />
-            <Text>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color="white" type='antdesign'/>
           </View>
         </TouchableOpacity>
     )}/>
