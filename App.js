df79bf357b2889ed2f799a1102b037ae8ee582a2
ethanialq <ethanialq@gmail.com>
2022-02-27 10:08:07 -0600

    implementación de la navegación entre pantallas

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import Home from './screens/Home';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <Home />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomeScreen' component={Home} options={{headerShown: false}}/>
+            <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false}}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import { Icon } from 'react-native-elements';
@@ -19,9 +20,10 @@ const data = [
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation()
   return (
     <FlatList data={data} horizontal keyExtractor={item => item.id} renderItem={({item}) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2`}>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2`} onPress={() => navigation.navigate(item.screen)}>
           <View>
             <Image style={styles.itemImage} source={{url: item.image}} />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
